d5f0f6fb7f074550052626e93ffb7be957659045
Akshay Srivastava <akshay.srivastava@globallogic.com>
2022-08-16 10:18:01 +0530

    authrization done

--- SweetShopProject/Controllers/InventoriesController.cs
+++ SweetShopProject/Controllers/InventoriesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SweetShopProject.Models;
 namespace SweetShop.Controllers
 {
+    
     public class InventoriesController : Controller
     {
         private readonly SweetContext _context;
